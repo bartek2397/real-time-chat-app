6a9023188c01d2af44c956f809a730b66c687d3f
Bartek <bartosz.wszolek97@gmail.com>
2024-07-04 18:31:29 +0200

    connecting db

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
 import Heading from './components/Heading';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+   axios.get('http://localhost:3500/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
   return (
     <div className="homePage">
       <header className='header'>
-        <Heading title='Welcome to the Chat!' subtitle='Please sign in to contact your friends' center />
+        <Heading title={message} subtitle='Please sign in to contact your friends' center />
       </header>
       <div className='formContainer'>
         <form action="" className='signUpForm'>
